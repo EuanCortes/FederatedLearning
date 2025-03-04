1ed2b9ed1e0a732c1da3849316edb71141058d8a
Hoflinger <phillhohwy2000@gmail.com>
2025-03-04 16:36:48 +0100

    updates

--- HPC/gpu_parallelism_test.py
+++ HPC/gpu_parallelism_test.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn
-from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader, Subset, random_split
+#from torchvision.datasets import CIFAR10
+#from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader, Subset
 from torch import optim
 import torch.multiprocessing as mp
 
@@ -25,15 +25,61 @@ if model_dir not in sys.path:
     sys.path.append(model_dir)  # add model to pythonpath
 
 from model import SmallCNN
+from dataset import load_data, split_data
 
 ############################################################
 # --------------------- End imports ---------------------- #
 ############################################################
 
-data_path = os.path.join(parent_dir, "data")
+
+def client_update(id, dataloader, epochs=10, lr=1e-3, criterion=nn.CrossEntropyLoss()):
+
+    device = torch.device(f"cuda:{id}")
+    net = SmallCNN().to(device)
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    for epoch in range(epochs):     # iterate thru local epochs
+            
+            epoch_loss = 0
+            for Xtrain, Ytrain in dataloader:     # iterate thru local data
+                Xtrain, Ytrain = Xtrain.to(device), Ytrain.to(device)
+
+                outputs = net(Xtrain)
+
+                optimizer.zero_grad()
+                loss = criterion(outputs, Ytrain)
+                loss.backward()
+                optimizer.step()
+                epoch_loss += loss.item() 
 
 
+if __name__ == "__main__":
 
-net = SmallCNN()
+    num_clients = 100
+    trainset, valset, testset = load_data(validation_percent=0.2)
+    client_indices = split_data(trainset, num_clients=num_clients, iid=True)
+     
+    mp.set_start_method('spawn', force=True)
+    manager = mp.Manager()
+    return_dict = manager.dict()
+    processes = []
 
-print(net)
+    print(f"num cuda devices: {torch.cuda.device_count()}")
+
+    for i in range(10):
+        print(f"training {i+1} clients on 1 gpu")
+        t1 = time.perf_counter()
+        dataloaders = [
+             DataLoader(Subset(trainset, client_indices[j]), batch_size=32, shuffle=True) for j in range(i+1)
+             ]
+        
+        for j, dataloader in enumerate(dataloaders):
+            cuda_id = j % torch.cuda.device_count()
+            p = mp.Process(target=client_update, args=(cuda_id, dataloader,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+        t2 = time.perf_counter()
+        print(f"Time taken to train {len(dataloaders)} client(s) on 1 gpu: {t2-t1:.3f} seconds\n")
--- model/dataset.py
+++ model/dataset.py
@@ -2,7 +2,7 @@ import torch
 import os
 
 from torchvision.datasets import CIFAR10
-from torch.utils.data import DataLoader, Subset, random_split
+from torch.utils.data import random_split
 import torchvision.transforms as transforms
 
 current_dir = os.path.dirname(os.path.realpath(__file__))   # current directory
@@ -19,6 +19,7 @@ def load_data(validation_percent=0.2, transform=default_transform):
     '''
     function for loading the dataset, and splitting into train, validation and test datasets
     '''
+    print("loading data:")
     dataset = CIFAR10(root=__DATA_DIR, train=True,
                 download=True, transform=transform)
     
@@ -26,11 +27,15 @@ def load_data(validation_percent=0.2, transform=default_transform):
     train_size = len(dataset) - val_size                # size of training dataset
 
     #generate random training and validation sets
-    trainset, valset = random_split(trainset, [train_size, val_size])
+    trainset, valset = random_split(dataset, [train_size, val_size])
 
     testset = CIFAR10(root=__DATA_DIR, train=False, 
                             download=True, transform=transform)
     
+    print(f"  number of training samples: {len(trainset)}")
+    print(f"  number of validation samples: {len(valset)}")
+    print(f"  number of test samples: {len(testset)}\n")
+
     return trainset, valset, testset
 
 
@@ -39,8 +44,10 @@ def split_data(data, num_clients=100, iid=True):
     '''
     method for splitting the data depending on the FL setting
     '''
+    print("splitting data:")
     if iid:
         client_indices = torch.tensor_split(torch.randperm(len(data)), num_clients)
+        print(f"  {num_clients} splits with {len(client_indices[0])} samples each\n")
     else:
         raise NotImplementedError("Non-IID sampling has not been implemented yet")
 
